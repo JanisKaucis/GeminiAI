40c5714a1b2d32771973085543e94104cf99d273
JanisKaucis <janiskaucis@gmail.com>
2025-08-14 17:06:32 +0300

    ADD: spinner for button

--- resources/js/pages/gemini/Index.vue
+++ resources/js/pages/gemini/Index.vue
@@ -6,9 +6,11 @@ import { ref } from 'vue';
 const question = ref('');
 const errors = ref({});
 const answer = ref('');
+const loading = ref(false);
 
 function submitForm() {
     errors.value = {};
+    loading.value = true;
     axios
         .post(route('ask-gemini', { question: question.value }))
         .then((response) => {
@@ -16,15 +18,18 @@ function submitForm() {
 
             if (response.status !== 200) {
                 errors.value = data.errors;
+                loading.value = false;
                 return;
             }
 
             answer.value = data.answer;
             question.value = '';
+            loading.value = false;
         })
         .catch((error) => {
             if (error.response?.status === 422) {
                 errors.value = error.response.data.errors;
+                loading.value = false;
             }
         });
 }
@@ -33,9 +38,10 @@ function submitForm() {
 <template>
     <div class="bg-black">
         <div class="flex h-screen w-1/2 flex-col justify-self-center bg-gray-400 p-4">
-            <div class="flex-1 overflow-hidden rounded bg-gray-100 shadow mb-2" v-if="answer">
-                <pre class="h-full overflow-x-hidden overflow-y-auto p-4 break-words whitespace-pre-wrap
-                text-black">{{ answer }}</pre>
+            <div class="mb-2 flex-1 overflow-hidden rounded bg-gray-100 shadow">
+                <div v-if="answer">
+                <pre class="h-full overflow-x-hidden overflow-y-auto p-4 break-words whitespace-pre-wrap text-black">{{ answer }}</pre>
+                </div>
             </div>
             <div class="mt-auto w-full rounded-lg bg-white p-6">
                 <form @submit.prevent="submitForm">
@@ -43,7 +49,10 @@ function submitForm() {
                         <input class="rounded-lg bg-gray-200 p-2 text-black" v-model="question" />
                         <p v-if="errors.question" class="text-sm text-red-600">{{ errors.question[0] }}</p>
                         <div class="flex justify-center">
-                            <Button class="m-4 bg-blue-300" type="submit">Ask Gemini</Button>
+                            <Button class="m-4 bg-blue-300 focus:bg-blue-400 flex content-center w-30 h-10" type="submit">
+                                <span v-if="loading"  class="w-5 h-5 border-4 border-white border-t-transparent rounded-full animate-spin"></span
+                                ><span v-if="!loading">Ask Gemini</span>
+                            </Button>
                         </div>
                     </div>
                 </form>
